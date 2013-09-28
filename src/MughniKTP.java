f4ef2c9a41c11d6f92e5f9b861eb651eb845b53e
unknown <klik.mughni@gmail.com>
2013-09-28 17:30:51 +0700

    commit04

---
+++ src/MughniKTP.java
@@ -0,0 +1,21 @@
+//Perintah penggunaan pakcage versi 01
+package Latihan2.sesi1.bin;
+
+public class MughniKTP
+{
+	public static void main(String[] data) {
+		System.out.println("NIK                 : 317.4030.7099.20007");
+		System.out.println("Nama                : MUGHNI SALAM");
+		System.out.println("Tempat/Tgl Lahir    : JAKARTA, 07-09-1992");
+		System.out.println("Alamat              : JLN MAMPANG PRAPATAN !$");
+		System.out.println("Jenis Kelamin       : LAKI - LAKI");
+		System.out.println("   RT / RW          : 012 / 001");
+		System.out.println("   Kel / Desa       : TEGAL PARANG");
+		System.out.println("   Kecamatan        : MAMPANG PRAPATAN");
+		System.out.println("Agama	            : ISLAM");
+		System.out.println("Status Perkawinan   : BELUM KAWIN");
+		System.out.println("Pekerjaan           : PELAJAR MAHASISWA");
+		System.out.println("Kewarganegaraan     : WNI");
+		System.out.println("Berlaku Hingga      : 07-09-2017");
+	}
+}
