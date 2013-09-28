fca7f7b738268dc97d29b496d93bcc63689fb350
unknown <klik.mughni@gmail.com>
2013-09-28 16:53:44 +0700

    commit03

---
+++ src/BiodataKu.java
@@ -0,0 +1,11 @@
+//Perintah penggunaan pakcage versi 01
+package Latihan.sesi1.bin;
+
+public class BiodataKu
+{
+	public static void main(String[] data) {
+		System.out.println("Nama  			: Mughni Salam");
+		System.out.println("Alamat  		: Mampang Prapatan 14");
+		System.out.println("Jenis Kelamin	: Cowok");
+	}
+}
