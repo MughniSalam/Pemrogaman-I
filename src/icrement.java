f97449164f6e6079db40e217028de64d55a1fd91
unknown <klik.mughni@gmail.com>
2013-10-26 21:02:23 +0700

    commit04

---
+++ src/icrement.java
@@ -0,0 +1,41 @@
+/**
+*class icrement.
+*@author (Adichan)
+*@version(op-02)
+*/
+
+public class icrement
+{
+	public static void main(String[] args)
+	{
+	
+		int a;
+		a=120;
+		System.out.println ("Contoh postfix increment : ");
+		System.out.println (a);
+		System.out.println (a++);
+		System.out.println (a);
+		a=120;
+		
+		System.out.println ("Contoh postfix increment : ");
+		System.out.println (a);
+		System.out.println (++a);
+		System.out.println (a);
+		a=120;
+		
+		System.out.println ("Contoh postfix increment : ");
+		System.out.println (a);
+		System.out.println (a--);
+		System.out.println (a);
+		a=120;
+		
+		System.out.println ("Contoh postfix increment : ");
+		System.out.println (a);
+		System.out.println (--a);
+		System.out.println (a);
+		a=120;
+		
+		}
+}
+
+		
